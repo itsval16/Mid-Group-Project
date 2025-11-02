37c73d69c0ec089df2c796e0a061c51c5f02338e
Valeria <76661272+itsval16@users.noreply.github.com>
2025-11-01 22:41:12 -0400

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,27 @@
+//
+//  Header.h
+//  Assigment3_Group3
+//
+//  Created by Valeria Pavia on 11/01/25.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+//________________________________________________________________
+// /*
+//Ali Roshdi
+//11/01/2025
+// Reflection -> What I found tricky for this assignment was splitting up the work initially, as we all just kind of went with the flow for
+// a little bit and then were able to properly figure things out as we went along. Another tricky part was figuring out how to use the vector
+// properly for the transaction history, as in order to constantly add the new transactions we'd have to implement the push_back function. As
+// well as having to convert the integers/doubles into strings, after some time I was able to put it all together using a stringstream and
+// template function. However, prior to coming across that solution, I was doing some research and came across something new that I learned
+// which was a function named 'to_string' that is in the <iomanip> library that automatically converts integers and doubles into strings. IBak
+// did not use that solution however, and ended up using the stringstream as I've mentioned earlier.
+// */
+
+
+
+
