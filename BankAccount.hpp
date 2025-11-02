5d98ea436b7f613627299d416c155b8c6b9f9f32
Valeria <76661272+itsval16@users.noreply.github.com>
2025-11-01 23:21:40 -0400

    Refactor comments in BankAccount.hpp for clarity
    Updated comments for clarity and improved code documentation.

--- BankAccount.hpp
+++ BankAccount.hpp
@@ -3,31 +3,29 @@
 // Date: October 30, 2025
 
 // BankAccount.h
-#ifndef BANKACCOUNT_H
+#ifndef BANKACCOUNT_H //ensure that this header file isn't repeated multiple times
 #define BANKACCOUNT_H
 
-#include <string>
-#include <vector>
+#include <string> //a string library that includes the name of the owner and the transaction strings
+#include <vector> //helps store th transaction hisroty
 using namespace std;
 
-class BankAccount {
-private:
-    string ownerName;
-    int accountNumber;
-    double balance;
-    vector<string> history;
+class BankAccount { //helps track the bank account's information to track the transactions going through
+private: //data protection
+    string ownerName; //stores the name of the owner
+    int accountNumber; //stores the account number
+    double balance; //helps store the balance of the account as the transactions are happening
+    vector<string> history; //allows for all the transaction history to be recorded for later, so it can be called back later to print in chronological order
+    string numToStr(double num) const; //helps convert the double value into a string so it can be displayed
 
-public:
-    BankAccount(const string& name, int accNum, double deposit);
-    void deposit(double amount);
-    bool withdraw(double amount);
-    double getBalance() const;
-    void display() const;
-    void showHistory() const;
+public: //helps interact with the bank account info
+    BankAccount(const string& name, int accNum, double deposit); //helps with storing and keeping track of the user's inputs
+    void deposit(double amount); //ensures a positive number is being entered
+    bool withdraw(double amount); //ensure that the withdrawal goes through successfully
+    double getBalance() const; //return the current baance of the account
+    void display() const; //shows the info needed to be shared with the user
+    void showHistory() const; //shares all info with the user
     
-private:
-    string numToStr(double num) const;
 };
 
-#endif
-
+#endif // end of the header guard
